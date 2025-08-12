cf8172a5c567d4c9a5b26ddcca56b3d8e2a795c4
aurora0x27 <qingfengzl0831@163.com>
2025-08-12 11:20:06 +0800

    fix installation

--- utils/presets.sh
+++ utils/presets.sh
@@ -2,6 +2,4 @@
 
 # Declare presets
 declare -A presets
-presets[darwin]="alacritty btop wezterm kitty karabiner ghostty tmux lsd zsh nvim nvim.vscode yazi"
-presets[arch]="alacritty wezterm kitty tmux lsd zsh nvim nvim.vscode yazi"
 presets[test]="aaa bbb ccc ddd"
